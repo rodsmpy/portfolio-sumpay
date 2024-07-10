80512c8738cbc9eebdf32a827b11a96a670a11e8
Rodney Sumpay <rodneypiojosumpay@gmail.com>
2024-07-10 22:00:49 +0800

    update browser router

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,19 @@
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Navbar from "./components/Navbar"
 import Home from "./pages/Home"
-import Skills from "./pages/Skills"
-import Contact from "./pages/Contact"
+
 function App() {
   return (
     <>
-      <Router>
-        <Navbar/>
+      <BrowserRouter basename="/portfolio-sumpay">
+        <Navbar />
         
 
         <Routes>
-          <Route path="/" element={<Home/>}/>
-          <Route path="/skills" element={<Skills/>}/> 
-          <Route path= "/contact" element={<Contact/>}/>        
+
+          <Route path="/" element={<Home/>}></Route>
         </Routes>
-      </Router>
+      </BrowserRouter>
 
     </>
   )
